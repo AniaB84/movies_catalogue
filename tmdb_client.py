4e0c7c5142f4adfe25324d644db8385b07e996d1
AniaB84 <ania76234@gmail.com>
2023-12-19 21:14:43 +0100

    This is a new commit for what I originally planned to be amended

--- tmdb_client.py
+++ tmdb_client.py
@@ -9,22 +9,51 @@ def get_popular_movies():
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
-get_popular_movies()
+def get_movies(how_many=8, list_type="popular"):
+    data = get_popular_movies()
+    return data["results"][:how_many]
+
+
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZGI3ZWVkMWEyMDYyNmE3OTkzZGU3NDVjNjAyMTFjZiIsInN1YiI6IjY1Nzc2ZTM4NGJmYTU0MDBmZTdmNTcyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XVEouCriGjxEOpKzsLMbcsmVUa6DT8AgHd_Hvpojurk"
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
 
 def get_movies(how_many):
     data = get_popular_movies()
     return data["results"][:8]
 
-how_many = []
-get_movies(how_many)
 
 
 def get_poster_url(poster_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_path}"
 
-poster_path = []
-get_poster_url(poster_path)
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
 
 
 
